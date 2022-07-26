4f75106f2e81a4854e3c5ede4ceb7203e2b463bb
pushkarshetye803@gmail.com <pushkarshetye803@gmail.com>
2022-07-26 08:14:56 +0530

    addede login, register and get balance apis

--- main.go
+++ main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	"github.com/gin-gonic/gin"
+	blogtypes "github.com/username/blog/x/blog/types"
 
 	"github.com/ignite-hq/cli/ignite/pkg/cosmosclient"
-	"github.com/username/blog/x/blog/types"
 )
+
+type UserCreateResponse struct {
+	Name     string "json:creator"
+	Address  string "json:title"
+	Mnemonic string "json:body"
+}
 
 type BlogPost struct {
 	Creator string "json:creator"
 	Title   string "json:title"
 	Body    string "json:body"
+}
+
+type NewUser struct {
+	Name string "json:name"
 }
 
 func main() {
@@ -26,11 +40,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	router.Use(validateUser(cosmos))
+	//router.Use(validateUser(cosmos))
+	router.POST("/login", userLogin)
+	router.POST("/register", userRegister)
 
-	router.GET("/posts", getAllPosts)
-	router.POST("/post", createPost)
-
+	router.GET("/posts", validateUser(cosmos), getAllPosts)
+	router.POST("/post", validateUser(cosmos), createPost)
+	router.GET("/myBalance", validateUser(cosmos), fetchBalance)
 	router.Run("localhost:8080")
 
 }
@@ -58,7 +74,38 @@ func validateUser(cosmos cosmosclient.Client) gin.HandlerFunc {
 		c.Set("authToken", authTokenStr)
 		c.Set("userAddress", address.String())
 		c.Next()
+
 	}
+}
+
+func fetchBalance(c *gin.Context) {
+	cosmos, err := cosmosclient.New(context.Background())
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"Message": "Failed To Get Balance"})
+		return
+	}
+	b, err := getBalance(c, cosmos)
+	c.JSON(http.StatusOK, gin.H{"Balance": b})
+}
+
+func userLogin(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"Message": "Login Sucess"})
+}
+
+func userRegister(c *gin.Context) {
+
+	var newUser NewUser
+	c.BindJSON(&newUser)
+	cmd := exec.Command("blogd", "keys", "add", newUser.Name, "--output", "json")
+	buf, err := cmd.CombinedOutput()
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"Message": "Register Failed"})
+		return
+	}
+	op := string(buf)
+	fmt.Println(op)
+
+	c.JSON(http.StatusOK, gin.H{"Message": "Register Failed", "op": op})
 }
 
 func createPost(c *gin.Context) {
@@ -73,7 +120,7 @@ func createPost(c *gin.Context) {
 	var iPost BlogPost
 	c.BindJSON(&iPost)
 
-	msg := &types.MsgCreatePost{
+	msg := &blogtypes.MsgCreatePost{
 		Creator: c.GetString("userAddress"),
 		Title:   iPost.Title,
 		Body:    iPost.Body,
@@ -95,11 +142,19 @@ func getAllPosts(c *gin.Context) {
 		log.Fatal(err)
 	}
 
-	queryClient := types.NewQueryClient(cosmos.Context)
-	queryResp, err := queryClient.Posts(context.Background(), &types.QueryPostsRequest{})
+	queryClient := blogtypes.NewQueryClient(cosmos.Context)
+	queryResp, err := queryClient.Posts(context.Background(), &blogtypes.QueryPostsRequest{})
 	if err != nil {
 		log.Fatal(err)
 	}
 	c.IndentedJSON(http.StatusOK, queryResp)
 
+}
+
+func getBalance(c *gin.Context, cosmos cosmosclient.Client) (string, error) {
+	resp, err := banktypes.NewQueryClient(cosmos.Context).Balance(c, &banktypes.QueryBalanceRequest{
+		Address: c.GetString("userAddress"),
+		Denom:   "stake",
+	})
+	return resp.Balance.String(), err
 }
